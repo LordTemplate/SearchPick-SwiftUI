733e3d8a9971935ea9e2e8f7b4efcf19b1c2f988
Alex Hernandez <ivan.hdez1984@gmail.com>
2025-02-07 14:07:18 -0800

    Initial Commit

---
+++ CodeChallenge/CodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeApp.swift
+//  CodeChallenge
+//
+//  Created by Ivan Alejandro Hernandez Sanchez on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
